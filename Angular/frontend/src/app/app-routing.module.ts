2c4425f7738ba3fde21f0481292e02b28f595270
Andrzin <69975897+Andrzin@users.noreply.github.com>
2021-11-19 23:36:43 -0300

    Add files via upload

---
+++ Angular/frontend/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from "./views/home/home.component";
+import { FavoritosComponent } from './views/favoritos/favoritos.component';
+
+const routes: Routes = [
+
+  {
+    path: "",
+    component: HomeComponent,
+  },
+  {
+    path: "favoritos",
+    component: FavoritosComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
